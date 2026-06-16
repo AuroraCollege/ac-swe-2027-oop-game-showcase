ddcc0d5daa389417460cfff87aebd535cd02e420
mkelly133 <92009909+mkelly133@users.noreply.github.com>
2026-06-16 04:30:27 +0000

    Added minesweeper to game library

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template, request
 from wumpus import HuntTheWumpus
 from ScissorsPaperRock import ScissorsPaperRock
+
+from minesweeper import MinesweeperGame # Import MrKelly's Game
+
 wumpus_game = HuntTheWumpus()
 scissors_paper_rock = ScissorsPaperRock()
+
+ms = MinesweeperGame() # Instantiate Mr Kell's Game
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,6 +23,7 @@ def wumpus():
         message = wumpus_game.new_game()
     return render_template('wumpus.html', message=message, game=wumpus_game)
 
+#Will N's Game
 @app.route('/ScissorsPaperRock', methods=["POST", "GET"])
 def scissorsPaperRock():
     if request.method == 'POST':
@@ -24,6 +31,19 @@ def scissorsPaperRock():
     else:
         message = ""
     return render_template('ScissorsPaperRock.html', game=scissors_paper_rock, choices=scissors_paper_rock.choices, scores=scissors_paper_rock.scores, message=message)
+
+# Mr Kelly's Game
+@app.route('/minesweeper', methods=['POST', 'GET'])
+def minesweeper():
+    if request.method == 'POST':
+        size = int(request.form['size'])
+        mines = int(request.form['mines'] )
+        ms.newGame(size, mines)
+    else:
+        id = request.args.get('id')
+        if id:
+            ms.click(id)
+    return render_template('minesweeper.html', game=ms)
 
 if __name__ == "__main__":
     app.run()
